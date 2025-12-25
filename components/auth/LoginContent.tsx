638fbffdbdcd6b26299753252a22fbb1b0839151
BildeaCristi <bildea.sa.ioan@student.utcluj.ro>
2025-12-26 01:27:37 +0200

    Fix UI mismatch: Replace static LoginContent with interactive LoginForm component

--- components/auth/LoginContent.tsx
+++ components/auth/LoginContent.tsx
@@ -1,17 +1,11 @@
 "use client";
 
-import WalletLogin from "./WalletLoginButton";
-import GoogleLoginButton from "./GoogleLoginButton";
+import LoginForm from "./LoginForm";
 
 export default function LoginContent() {
     return (
-        <div className="flex flex-col gap-8 justify-center items-center py-4">
-            <div className="flex flex-col gap-4 justify-center items-center w-full">
-                <span className="text-lg font-medium">Connect as guest</span>
-                <GoogleLoginButton/>
-                <span className="text-lg font-medium">Connect as user</span>
-                <WalletLogin/>
-            </div>
+        <div className="w-full max-w-md mx-auto">
+            <LoginForm />
         </div>
     );
 }
